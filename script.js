f34f1d9003b4276df8cfba23cb414c5c2264d6cf
tommyfm123 <tomasf.m@icloud.com>
2023-05-11 17:56:18 -0300

    carrousel Change

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ navLinks.forEach(link => {
 });
 
 
-//swiper 
+// type effect
+
+var i = 0;
+var txt = 'Soy tomas Fernandez Murga'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
  
